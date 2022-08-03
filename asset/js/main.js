ae316f887460254997b72d4e02d3929fd1ae3809
mimbabu20 <mimbabu20@gmail.com>
2022-08-03 15:13:19 +0600

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+
+// let nav = document.querySelector(".navbar");
+// window.onscroll = function (){
+
+//     if(document.documentElement.scrollTop>20){
+
+//         nav.classList.add("header-scrolled");
+//      } else{
+//             nav.classList.remove("header-scrolled");
+//         }
+//     }
+
+
+
+// window.alert('Stop the door');
